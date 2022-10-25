e2b3257399af57f47d190eb21ee2ebf626374e2f
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-10-25 17:39:58 +0900

    20221025 프로젝트

--- src/test/java/com/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/book/springboot/web/HelloControllerTest.java
@@ -1,11 +1,11 @@
-package com.book.springboot;
+package com.book.springboot.web;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -14,13 +14,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @WebMvcTest(controllers = HelloControllerTest.class)
 public class HelloControllerTest {
     @Autowired
-    private  MockMvc mvc;
+    private MockMvc mvc;
 
     @Test
-    public  void  hello가_리턴된다() throws Exception{
-        String hello="hello";
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
 
-        mvc.perform(get("/hello")).andExpect(status().isOk()).andExpect(content().string(hello));
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
     }
 
 }
