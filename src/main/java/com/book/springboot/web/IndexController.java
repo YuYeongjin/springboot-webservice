50261afd03b38ad1f9c5283d9baecf4fa3099cc8
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-10-31 16:43:45 +0900

    20221031 aws로 연동 전

--- src/main/java/com/book/springboot/web/IndexController.java
+++ src/main/java/com/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.book.springboot.web;
 
+import com.book.springboot.config.auth.LoginUser;
 import com.book.springboot.config.auth.dto.SessionUser;
 import com.book.springboot.service.posts.PostsService;
 import com.book.springboot.web.dto.PostsResponseDto;
@@ -19,11 +20,10 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
-        model.addAttribute("posts",postsService.findAllDesc());
-        SessionUser user=(SessionUser) httpSession.getAttribute("user");
-        if(user!=null){
-            model.addAttribute("userName",user.getName());
+    public String index(Model model, @LoginUser SessionUser user) {
+        model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
         }
         return "index";
     }
