63feb0a5df81088b665297856f460a729a002248
YuYeongjin <108460833+YuYeongjin@users.noreply.github.com>
2022-11-03 11:32:21 +0900

    20221103 배포전오류찾기

--- src/main/java/com/book/springboot/config/auth/SecurityConfig.java
+++ src/main/java/com/book/springboot/config/auth/SecurityConfig.java
@@ -2,12 +2,10 @@ package com.book.springboot.config.auth;
 
 import com.book.springboot.domain.user.Role;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.SecurityFilterChain;
 
 @RequiredArgsConstructor
 @Configuration
@@ -33,14 +31,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .oauth2Login()
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService);
-    }
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .oauth2Login();
-        return http.build();
     }
 
 }
